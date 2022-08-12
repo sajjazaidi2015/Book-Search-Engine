af44b14be28e48b73a6bbd9ffe2596defa2e68bb
Sajjad Zaidi <sajjadzaidi2015@gmail.com>
2022-08-12 11:05:22 -0400

    update the auth file

--- server/server.js
+++ server/server.js
@@ -50,7 +50,7 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const { ApolloServer } = require('apollo-server-express');
-const { authMiddleware } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 const { typeDefs, resolvers } = require('./schemas');
 
 // express port setup
